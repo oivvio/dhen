f3b60e8c3f1c26d40c877e8e2301d9abfc5e0a05
Oivvio Polite <oivvio@blomma.(none)>
2012-09-12 01:05:40 +0200

    added bootstrap

--- dhen_naive.js
+++ dhen_naive.js
@@ -1,15 +1,23 @@
 var http = require('http');
 var fs = require('fs');
+
+
+
 //var util = require('util');
 //var events = require('events');
 //var StringDecoder = require('string_decoder').StringDecoder;
 //var decoder = new StringDecoder('utf8');
 
 //var proxyhost = "http://127.0.0.1:8080";
-var proxyport = 80; 
+var proxyport = 8080; 
 var target = "www.dn.se";
+var projectfolder = "/home/oivvio/dhen";
 //var target = "oivviosarkiv.polite.se";
 //var target = "forskjutningar.se";
+
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
 
 http.createServer(function(request, response) {
 
@@ -17,118 +25,131 @@ http.createServer(function(request, response) {
     var pattern = RegExp("\\.");
 
     if ( /om_dhen/.test(request.url) ) {
-    
-            var fn = "/home/oivvio/dhen" + request.url;
-            console.log("get me " + fn);
-            fs.readFile(fn, function(error, content) {
-                if (error) {
+
+
+        if (request.url == "/om_dhen" || request.url == "/om_dhen/") {
+            var fn = projectfolder +  "/om_dhen/index.html";
+
+
+        } else {
+            var fn = projectfolder + request.url; 
+        }
+
+
+
+        console.log("get me " + fn);
+
+        fs.readFile(fn, function(error, content) {
+            if (error) 
+                {
                     response.writeHead(500);
                     response.end();
-}
-    else if (pattern.test(request.url))
-        {
-
-            response.writeHead(302, {'Location' :  "http://" + target  + request.url});
-            response.end();
-        }
-
-        //else if (  request.url == "/om_dhen" || request.url == "/om_dhen/" ) {
-        else if ( /om_dhen/.test(request.url) ) {
-
-        
                 }
                 else {
+                    var contenttype = "text/html";
+                    if (fn.endsWith(".css")) {
+                        var contenttype = "text/css";
+                    } 
+                    else if (fn.endsWith(".js")) {
+                        var contenttype = "text/javascript";
+                    }
 
-                    response.writeHead(200, { 'Content-Type': 'text/html' });
+                    response.writeHead(200, { 'Content-Type': contenttype });
                     response.end(content, 'utf-8');
                 }
-            });
-        } 
+        });
+    }
 
-        else {
+    else if (pattern.test(request.url)) {
 
+        response.writeHead(302, {'Location' :  "http://" + target  + request.url});
+        response.end();
+    }
 
-            var proxy = http.createClient(80, target)
-
-            // look at the incoming request. If it's not for our server redirect
-            // if it's not html redirect
-            console.log(new Date() + "," + new Date().getTime(), "," + request.connection.remoteAddress + "," + request.url);
-            var proxy_request = proxy.request(request.method, request.url, {});
+    else {
 
 
-            //Adding a listner for event "response"
-            proxy_request.addListener('response', function (proxy_response) {
+        var proxy = http.createClient(80, target)
 
-                //var decoder = new StringDecoder('utf8');
-                var data = "";   
-                //Every time "data" happens add the chunk to our data
-
-                proxy_response.addListener('data', function(chunk) {
-                    //response.write(chunk, 'binary');
-                    //var chunkstr = decoder.write(chunk);
-
-                    //util.log("len: " + chunkstr.length);
-                    //data += chunkstr;
-                    data += chunk;
-
-                });
-
-                //When "end" happens trigger "end" on response
-                proxy_response.addListener('end', function() {
-
-                    //at the end write all our data out
-
-                    //Before that data goes on the wire let's do some replacements
-                    //
-
-                    //var t1 = new Date().getTime();
-                    data = data.replace(/\bhan\b/g, "hen");
-                    data = data.replace(/\bhon\b/g, "hen");
-
-                    data = data.replace(/\bhonom\b/g, "henom");
-                    data = data.replace(/\bhenne\b/g, "henom");
-
-                    data = data.replace(/\bhans\b/g, "hens");
-                    data = data.replace(/\bhennes\b/g, "hens");
-
-                    data = data.replace(/\bman\b/g, "en");
-
-                    data = data.replace(/\bHan\b/g, "Hen");
-                    data = data.replace(/\bHon\b/g, "Hen");
-
-                    data = data.replace(/\bHonom\b/g, "Henom");
-                    data = data.replace(/\bHenne\b/g, "Henom");
-
-                    data = data.replace(/\bHans\b/g, "Hens");
-                    data = data.replace(/\bHennes\b/g, "Hens");
-
-                    data = data.replace(/\bMan\b/g, "En");
-
-                    data = data.replace(/\bDN\b/g, "DHEN");
+        // look at the incoming request. If it's not for our server redirect
+        // if it's not html redirect
+        console.log(new Date() + "," + new Date().getTime(), "," + request.connection.remoteAddress + "," + request.url);
+        var proxy_request = proxy.request(request.method, request.url, {});
 
 
-                    response.write(data, 'utf8');
+        //Adding a listner for event "response"
+        proxy_request.addListener('response', function (proxy_response) {
 
+            //var decoder = new StringDecoder('utf8');
+            var data = "";   
+            //Every time "data" happens add the chunk to our data
 
-                    response.end();
-                });
+            proxy_response.addListener('data', function(chunk) {
+                //response.write(chunk, 'binary');
+                //var chunkstr = decoder.write(chunk);
 
+                //util.log("len: " + chunkstr.length);
+                //data += chunkstr;
+                data += chunk;
 
-                response.writeHead(proxy_response.statusCode, proxy_response.headers);
             });
 
+            //When "end" happens trigger "end" on response
+            proxy_response.addListener('end', function() {
 
-            request.addListener('data', function(chunk) {
-                //We can comment the next line out
-                proxy_request.write(chunk, 'binary');
+                //at the end write all our data out
+
+                //Before that data goes on the wire let's do some replacements
+                //
+
+                //var t1 = new Date().getTime();
+                data = data.replace(/\bhan\b/g, "hen");
+                data = data.replace(/\bhon\b/g, "hen");
+
+                data = data.replace(/\bhonom\b/g, "henom");
+                data = data.replace(/\bhenne\b/g, "henom");
+
+                data = data.replace(/\bhans\b/g, "hens");
+                data = data.replace(/\bhennes\b/g, "hens");
+
+                data = data.replace(/\bman\b/g, "en");
+
+                data = data.replace(/\bHan\b/g, "Hen");
+                data = data.replace(/\bHon\b/g, "Hen");
+
+                data = data.replace(/\bHonom\b/g, "Henom");
+                data = data.replace(/\bHenne\b/g, "Henom");
+
+                data = data.replace(/\bHans\b/g, "Hens");
+                data = data.replace(/\bHennes\b/g, "Hens");
+
+                data = data.replace(/\bMan\b/g, "En");
+
+                data = data.replace(/\bDN\b/g, "DHEN");
+
+
+                response.write(data, 'utf8');
+
+
+                response.end();
             });
 
 
-            request.addListener('end', function() {
-                //util.log("a request end evetn");
-                proxy_request.end();
-            });
-        }
+            response.writeHead(proxy_response.statusCode, proxy_response.headers);
+        });
+
+
+        request.addListener('data', function(chunk) {
+            //We can comment the next line out
+            proxy_request.write(chunk, 'binary');
+        });
+
+
+        request.addListener('end', function() {
+            //util.log("a request end evetn");
+            proxy_request.end();
+        });
+    }
 
 
 }).listen(proxyport);
